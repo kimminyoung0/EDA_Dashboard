8c21454dbc64bc73e853efa26ed4006459718957
kimminyoung0 <kmy5774@naver.com>
2025-07-02 23:55:49 +0900

    Modify script

--- app/eda_dashboard.py
+++ app/eda_dashboard.py
@@ -36,13 +36,13 @@ def sanitize_object_columns(df):
 
 # GA4 태그 삽입
 components.html("""
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-34GY62HL58"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-34GY62HL58');
+<!-- Microsft Clarity -->
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "s8ns1np3w5");
 </script>
 """, height=0)
 
